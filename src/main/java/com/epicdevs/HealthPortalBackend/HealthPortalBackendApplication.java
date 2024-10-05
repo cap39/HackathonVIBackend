8ab2b724c9b545beb4f4da591ae99dfa69355644
cap39 <buzzkill619@gmail.com>
2024-10-05 14:44:49 -0500

    add backend

---
+++ src/main/java/com/epicdevs/HealthPortalBackend/HealthPortalBackendApplication.java
@@ -0,0 +1,13 @@
+package com.epicdevs.HealthPortalBackend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HealthPortalBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HealthPortalBackendApplication.class, args);
+	}
+
+}
